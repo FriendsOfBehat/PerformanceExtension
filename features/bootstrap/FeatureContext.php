3cf2cbb83a44bdac6f859a133d3b525c6f2c6fe1
Kamil Kokot <kamil@kokot.me>
2016-07-22 20:38:06 +0200

    Run Behat using the extension in order to test it

---
+++ features/bootstrap/FeatureContext.php
@@ -0,0 +1,22 @@
+<?php
+
+use Behat\Behat\Context\Context;
+
+/**
+ * @author Kamil Kokot <kamil@kokot.me>
+ */
+final class FeatureContext implements Context
+{
+    /**
+     * @Given I left alone
+     * @Given my mind was blank
+     * @Given I needed time to think
+     * @When I get the memories from my mind
+     * @Then the evil face should twist my mind
+     * @Then it should bring me to despair
+     */
+    public function doNothing()
+    {
+
+    }
+}
---
+++ features/iron_maiden.feature
@@ -0,0 +1,12 @@
+Feature: Iron Maiden
+    In order to learn how to write features
+    As a young BDD padawan
+    I want to translate lyrics into scenarios
+
+    Scenario: The Number Of The Beast
+        Given I left alone
+        And my mind was blank
+        And I needed time to think
+        When I get the memories from my mind
+        Then the evil face should twist my mind
+        And it should bring me to despair
