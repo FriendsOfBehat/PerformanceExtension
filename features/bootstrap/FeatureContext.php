414719f235f13db4456b799be6912671495eaddf
Kamil Kokot <kamil@kokot.me>
2016-07-22 21:02:06 +0200

    Add test to prevent regression on unpacking an array with string keys

--- features/bootstrap/FeatureContext.php
+++ features/bootstrap/FeatureContext.php
@@ -14,9 +14,32 @@ final class FeatureContext implements Context
      * @When I get the memories from my mind
      * @Then the evil face should twist my mind
      * @Then it should bring me to despair
+     * @Then I should take yours too
      */
     public function doNothing()
     {
 
+    }
+
+    /**
+     * @When you take my :life
+     */
+    public function doNothingButWithAnArgument($life)
+    {
+        // This Turnip pattern is transformed into:
+        //   /^you take my ["']?(?P<life>(?<=")[^"]*(?=")|(?<=')[^']*(?=')|\-?[\w\.\,]+)['"]?$/i
+        // Then the transformer is trying to be called with arguments:
+        //   ['life' => 'life']
+        // But our custom CallHandler tries to unpack that array with `...`, which results in:
+        //   Fatal error: Cannot unpack array with string keys
+        // So this scenario checks whether `array_values()` is called on arguments before executing the call.
+    }
+
+    /**
+     * @Transform :life
+     */
+    public function transformLifeToDeath($life)
+    {
+        return 'death';
     }
 }
--- features/iron_maiden.feature
+++ features/iron_maiden.feature
@@ -3,10 +3,14 @@ Feature: Iron Maiden
     As a young BDD padawan
     I want to translate lyrics into scenarios
 
-    Scenario: The Number Of The Beast
+    Scenario: The Number Of The Beast (basic functionality)
         Given I left alone
         And my mind was blank
         And I needed time to think
         When I get the memories from my mind
         Then the evil face should twist my mind
         And it should bring me to despair
+
+    Scenario: The Trooper (cannot unpack array with string keys - usage of an argument transformer)
+        When you take my life
+        Then I should take yours too
